beedbaf91e3f1fcda225d93e63a74a3304e6071a
jmartin111 <chess.logic@gmail.com>
2021-11-03 12:19:34 -0700

    Initial commit
    * EDIT: remove django secret key

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yat4ea^65k*an2*zz8jbxztz3+ri+6vp4)z9npu-b0e=gax6ek'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
